7f4537dd44a8ae59d095df5318618891f2115e67
mmihye <smilehye910@naver.com>
2022-11-12 10:09:44 +0900

    friendList & uploadPost layout

---
+++ app/src/main/java/com/android/example/hongseokchun/model/User.kt
@@ -0,0 +1,9 @@
+package com.android.example.hongseokchun.model
+
+data class User (
+    val email: String = "",
+    val name: String = "",
+    val date: String= "",
+    val question: String ="",
+    val answer: String=""
+)
---
+++ app/src/main/java/com/android/example/hongseokchun/viewmodel/UserViewModel.kt
@@ -0,0 +1,12 @@
+package com.android.example.hongseokchun.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.example.hongseokchun.model.User
+import com.android.example.hongseokchun.repository.UserRepository
+
+class UserViewModel : ViewModel(){
+
+}
