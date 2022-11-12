6d23a2757d6b343b831efa7694fb41c3b4ad91f5
mmihye <smilehye910@naver.com>
2022-11-12 10:09:44 +0900

    friendList & uploadPost layout

---
+++ app/src/main/java/com/android/example/hongseokchun/repository/UserRepository.kt
@@ -0,0 +1,30 @@
+package com.android.example.hongseokchun.repository
+
+import android.content.ContentValues
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.example.hongseokchun.model.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+class UserRepository {
+    fun getData(): LiveData<User> {
+        val db = Firebase.firestore
+        val mutableData = MutableLiveData<User>()
+
+
+        db.collection("users").document("hongseokchun@naver.com").get()
+            .addOnSuccessListener { documentSnapshot ->
+                val data = documentSnapshot.toObject<User>()
+                mutableData.value=data!!
+                Log.d("ingred repo",data.toString())
+            }
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "get failed with ", exception)
+            }
+
+        return mutableData
+    }
+}
