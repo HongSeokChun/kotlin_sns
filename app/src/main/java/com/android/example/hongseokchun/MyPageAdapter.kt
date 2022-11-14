35ce7ab26d11e9eba05f08d98d5beb174fdb8781
Hong <thgks779@naver.com>
2022-11-14 20:21:10 +0900

    init2

---
+++ app/src/main/java/com/android/example/hongseokchun/MyPageAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.recyclerview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.databinding.MyPageItemBinding
+
+//data class Student(val uid: Int, val name: String)
+//data class Post(val uid: Int,val time: String, val like: String, val comment: String)
+
+class MyPageViewHolder(val binding: MyPageItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+class MyPageAdapter(private val context: Context, private val posts: MutableList<Post>) : RecyclerView.Adapter<MyPageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MyPageItemBinding.inflate(inflater, parent, false)
+        return MyPageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyPageViewHolder, position: Int) {
+        val post = posts[position]
+
+        holder.binding.myPagePostImage.setImageResource(R.drawable.sample)
+        holder.binding.myPagePostTitle.text = "MY POST"
+        holder.binding.myPagePostComment.text = "댓글 12개"
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+}
